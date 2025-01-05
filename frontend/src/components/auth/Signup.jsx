f0e3b97f82c62575c817d75e6aeae0deb2d212c1
NabanitaSarker <nabanitasarker18@gmail.com>
2025-01-05 16:42:17 +0600

    modified

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -8,30 +8,30 @@ import { Link } from 'react-router-dom'
 
 const Signup = () => {
     const [input, setInput] = useState({
-        fullname:"",
-        email:"",
-        phonenumber:"",
-        password:"",
-        role:"",
-        file:""
+        fullname: "",
+        email: "",
+        phonenumber: "",
+        password: "",
+        role: "",
+        file: ""
 
     });
     const changeEventHandler = (e) => {
-      setInput({...input, [e.target.name]:e.target.value});
+        setInput({ ...input, [e.target.name]: e.target.value });
     }
-    
+
     const changeFileHandler = (e) => {
-      setInput({...input, file:e.target.files?.[0]});
+        setInput({ ...input, file: e.target.files?.[0] });
     }
 
     return (
         <div>
             <Navbar />
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
-             <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
                     <h1 className='font-bold text-xl mb-5'>Sign Up</h1>
-             
-                   
+
+
                     <div className='my-2'>
                         <Label>Full Name</Label>
                         <Input type="text"
@@ -54,18 +54,18 @@ const Signup = () => {
                         <RadioGroup className="flex items-center gap-4 my-5">
                             <div className="flex items-center space-x-2">
                                 <Input
-                                type ="radio"
-                                name="role"
-                                value="student"
-                                className="cursor-pointer" />
+                                    type="radio"
+                                    name="role"
+                                    value="student"
+                                    className="cursor-pointer" />
                                 <Label htmlFor="r1">Student</Label>
                             </div>
                             <div className="flex items-center space-x-2">
                                 <Input
-                                type ="radio"
-                                name="role"
-                                value="tutor"
-                                className="cursor-pointer"/>
+                                    type="radio"
+                                    name="role"
+                                    value="tutor"
+                                    className="cursor-pointer" />
                                 <Label htmlFor="r2">Tutor</Label>
                             </div>
 
