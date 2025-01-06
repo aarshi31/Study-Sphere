07a4f7e27d0b292a48fcd61fa802a01b253b2d6f
NabanitaSarker <nabanitasarker18@gmail.com>
2025-01-06 17:30:59 +0600

    Update of last sprint

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,22 +1,26 @@
-// import React from 'react'
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { RadioGroup } from '../ui/radio-group'
 import { Button } from '../ui/button'
-import { Link } from 'react-router-dom'
-import { toast, Toaster } from 'sonner';
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { toast } from 'sonner'
+import { useDispatch, useSelector } from 'react-redux'
+import { setLoading, setUser } from '@/redux/authSlice'
+import { Loader2 } from 'lucide-react'
 
 const Login = () => {
     const [input, setInput] = useState({
-
         email: "",
-        phonenumber: "",
         password: "",
         role: "",
     });
+    const { loading,user } = useSelector(store => store.auth);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const changeEventHandler = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value });
@@ -24,54 +28,56 @@ const Login = () => {
 
     const submitHandler = async (e) => {
         e.preventDefault();
-
-
         try {
+            dispatch(setLoading(true));
             const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                withCredentials: true
+                withCredentials: true,
             });
             if (res.data.success) {
+                dispatch(setUser(res.data.user));
                 navigate("/");
                 toast.success(res.data.message);
-
             }
-        }
-
-        catch (error) {
+        } catch (error) {
             console.log(error);
             toast.error(error.response.data.message);
-
+        } finally {
+            dispatch(setLoading(false));
         }
-
     }
+    useEffect(()=>{
+        if(user){
+            navigate("/");
+        }
+    },[])
     return (
         <div>
             <Navbar />
-            <div className='flex items-center justify-center min-h-screen max-w-7xl mx-auto'>
-                <form onSubmit={submitHandler} className='w-1/10 border border-gray-200 rounded-md p-4 my-10'>
-                    <h1 className='font-bold text-xl mb-5'>Log In</h1>
-
-
-
+            <div className='flex items-center justify-center max-w-7xl mx-auto'>
+                <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                    <h1 className='font-bold text-xl mb-5'>Login</h1>
                     <div className='my-2'>
                         <Label>Email</Label>
-                        <Input type="email"
+                        <Input
+                            type="email"
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="eshat@gmail.com"
+                            placeholder="patel@gmail.com"
                         />
                     </div>
+
                     <div className='my-2'>
                         <Label>Password</Label>
-                        <Input type="password"
+                        <Input
+                            type="password"
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="Enter your password"
+                            placeholder="patel@gmail.com"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
@@ -83,25 +89,27 @@ const Login = () => {
                                     value="student"
                                     checked={input.role === 'student'}
                                     onChange={changeEventHandler}
-                                    className="cursor-pointer" />
+                                    className="cursor-pointer"
+                                />
                                 <Label htmlFor="r1">Student</Label>
                             </div>
                             <div className="flex items-center space-x-2">
                                 <Input
                                     type="radio"
                                     name="role"
-                                    value="tutor"
-                                    checked={input.role === 'tutor'}
+                                    value="recruiter"
+                                    checked={input.role === 'recruiter'}
                                     onChange={changeEventHandler}
-                                    className="cursor-pointer" />
-                                <Label htmlFor="r2">Tutor</Label>
+                                    className="cursor-pointer"
+                                />
+                                <Label htmlFor="r2">Recruiter</Label>
                             </div>
-
                         </RadioGroup>
-
                     </div>
-                    <Button type="submit" className="w-full my-4">Log In</Button>
-                    <span className="text-sm">Don't have an account? <Link to="/signup" className='text-blue-600'>Sign Up</Link></span>
+                    {
+                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Login</Button>
+                    }
+                    <span className='text-sm'>Don't have an account? <Link to="/signup" className='text-blue-600'>Signup</Link></span>
                 </form>
             </div>
         </div>
